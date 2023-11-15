4ddf6273149a82a4a74ad2c84b16d349e72e4e8a
AllyTaylor96 <allytaylor@hotmail.co.uk>
2023-11-15 17:39:42 +0000

    bones of data prep and dataset loading in

--- src/train.py
+++ src/train.py
@@ -1,6 +1,7 @@
 import os
 import argparse
-from train_data import retrieve_dataset, preprocess_dataset
+import fasttext.util
+from train_data import retrieve_dataset, preprocess_dataset, ToxicDataset
 
 
 """ Main driver function for training. Will go through the steps in sequence.
@@ -18,12 +19,38 @@ Can potentially use model on Twitch comments etc. to ascertain toxicity? An addi
 def main():
 
     # data retrieval and prep
+    print('retrieving datasets...')
     train_dataset, val_dataset, test_dataset = retrieve_dataset()
-    processed_train = preprocess_dataset(train_dataset)
-    processed_val = preprocess_dataset(train_dataset)
-    processed_test = preprocess_dataset(train_dataset)
 
-    print('Hello world!')
+    # data cleaning
+    print('cleaning datasets...')
+    processed_train = preprocess_dataset(train_dataset)
+    processed_val = preprocess_dataset(val_dataset)
+    processed_test = preprocess_dataset(test_dataset)
+
+    # download the FastText model (caution, is 4GB in size)
+    print('grabbing FastText base English encoder...')
+    fasttext.util.download_model('en', if_exists='ignore')
+
+    # loading the FastText model
+    print('loading FastText model...')
+    ft = fasttext.load_model('cc.en.300.bin')
+
+    # reduce FT dimension to 100 as unnecessary for 300
+    fasttext.util.reduce_model(ft, 100)
+
+
+    # pass into Pytorch datasets
+    print('creating Torch Datasets...')
+    trainingDataset = ToxicDataset(processed_train, ft)
+    print('How many comments in training: ', trainingDataset.__len__())
+    valDataset = ToxicDataset(processed_val, ft)
+    print('How many comments in validation: ', valDataset.__len__())
+    testDataset = ToxicDataset(processed_test, ft)
+    print('How many comments in testing: ', testDataset.__len__())
+
+    # now we move to training the actual model
+    print('Model training begins...')
 
 if __name__ == "__main__":
     main()
--- src/train_data.py
+++ src/train_data.py
@@ -1,4 +1,6 @@
 import os
+import re
+import string
 import datasets
 import torch
 from torch.utils.data import Dataset
@@ -8,40 +10,46 @@ print(torch.cuda.is_available())
 
 def retrieve_dataset():
     full_dataset = datasets.load_dataset("OxAISH-AL-LLM/wiki_toxic")
-    train_dataset = full_dataset['balanced_train']
+    train_dataset = full_dataset['train']
     val_dataset = full_dataset['validation']
     test_dataset = full_dataset['test']
 
     return train_dataset, val_dataset, test_dataset
 
+def normalise(example):
+    """ Clean text from dataset and append to new column. """
+
+    # text cleaning
+    norm_text = example['comment_text'].lower()
+    exclude = set(string.punctuation)
+    norm_text = re.sub(r"\[.*?]", "", norm_text)
+    norm_text = ''.join(ch for ch in norm_text if ch not in exclude)
+    norm_text = norm_text.replace('\n', '')
+    example['norm_comment_text'] = norm_text
+
+    return example
+
 
 def preprocess_dataset(dataset_obj):
-    """Convert text files to tensors using Fasttext?"""
-    print('processing...')
+    """Converts raw text from dataset into normalised clean sentences for encoding. """
+
+    dataset_obj = dataset_obj.map(normalise, batched=False, load_from_cache_file=True)
 
     return dataset_obj
 
-def vectorize(sentence):
-    """ Use FT to vectorize the sentences into embeddings."""
-
-    # need to load in fasttext model as part of docker image prep etc.
-    # ft = fasttext.load_model('cc.en.300.bin')
-    # sentence_embedding = ft.get_word_vector('hello')
-
-    return sentence_embedding
-
 
 class ToxicDataset(Dataset):
-    def __init__(self, dataset_obj):
+    def __init__(self, dataset_obj, ft_model):
         self.dataset = dataset_obj
+        self.ft_model = ft_model
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, idx):
-        sentence = self.dataset[idx]['comment_text']
+        sentence = self.dataset[idx]['norm_comment_text']
         label = self.dataset[idx]['label']
 
-        sentence_embedding = vectorize(sentence)
+        sentence_embedding = self.ft_model.get_sentence_vector(sentence)
 
         return sentence_embedding, label
