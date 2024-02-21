f6b5083acdcd16156cc577104d7e43c74df1ac04
AllyTaylor96 <allytaylor@hotmail.co.uk>
2024-02-21 16:59:18 +0000

    added docstrings to data prep, general cleaning + added optional clearing of cache

--- src/data_functions.py
+++ src/data_functions.py
@@ -13,92 +13,29 @@ import numpy as np
 from torch.utils.data import Dataset
 
 def retrieve_dataset(hf_dataset_name, data_dir):
+    """Load the HF dataset if cached, download if not."""
+
     hf_dataset_path = data_dir + '/hf_dataset'
     logging.info(f'HF Dataset cached at {hf_dataset_path}')
     full_dataset = datasets.load_dataset(hf_dataset_name, cache_dir=hf_dataset_path)
     return full_dataset
 
 
+def clear_dataset_cache(hf_dataset_name, data_dir):
+    """Clears original HF cache if requested."""
 
-
-
-def retrieve_word_vecs(data_dir, vec_url):
-
-    # define FastText URLs
-    vec_name = vec_url.split('/')[-1]
-
-    # check if word vectors already there
-    output_filepath = data_dir + '/' + vec_name
-
-
-    if os.path.isfile(output_filepath.replace('.zip', '')):
-        logging.info(f'Word vec file already found at {output_filepath} - skipping download...')
-
-    else:
-        # if not, download them from the FastText site
-        logging.info(f'Downloading Fasttext Vectors from {vec_url}...')
-
-        r = requests.get(vec_url, stream=True)
-        total_len = int(r.headers.get('content-length', 0))
-        block_size = 1024
-
-        # download in blocks using requests
-        with tqdm(total=total_len, unit='B', unit_scale=True) as progress_bar:
-            with open(output_filepath, 'wb') as f:
-                for data in r.iter_content(block_size):
-                    progress_bar.update(len(data))
-                    f.write(data)
-
-            if total_len != 0 and progress_bar.n != total_len:
-                logging.error("Could not download FastText Vectors")
-                raise RuntimeError("Could not download FastText Vectors")
-
-        # unzip file
-        logging.info('unzipping word vecs...')
-        shutil.unpack_archive(output_filepath, extract_dir=data_dir)
-
-    word_vec_path = output_filepath.replace('.zip', '')
-    return word_vec_path
-
-def clean_tokenize_text(example):
-    """ Clean + tokenize text from dataset and append to new columns. """
-
-    # cleaning
-    norm_text = example['comment_text'].lower()
-    exclude = set(string.punctuation)
-    norm_text = re.sub(r"\[.*?]", "", norm_text)
-    norm_text = ''.join(ch for ch in norm_text if ch not in exclude)
-    norm_text = norm_text.replace('\n', '')
-    example['norm_comment_text'] = norm_text
-
-    # tokenizing
-    tokenized_text = fasttext.tokenize(example['norm_comment_text'])
-    example['tokenized_text'] = tokenized_text
-
-    # get sentence length for filtering later
-    example['sentWordCount'] = len(example['tokenized_text'])
-
-    return example
-
-def encode_text(example, vocab_dict, max_sent_len):
-
-    tokenized_text = example['tokenized_text']
-
-    # pad the text out to max sent len
-    tokenized_text += ['<pad>'] * (max_sent_len - len(tokenized_text))
-
-    # encode the padded tokens to their IDs using the vocab dict
-    encoded_text = [vocab_dict.get(token, 1) for token in tokenized_text]
-
-    example['encoded_text'] = np.array(encoded_text)
-
-    return example
+    hf_dataset_path = data_dir + '/hf_dataset'
+    logging.info(f'Clearing HF dataset cached at {hf_dataset_path}')
+    full_dataset = datasets.load_dataset(hf_dataset_name, cache_dir=hf_dataset_path)
+    num_cleared_files = full_dataset.cleanup_cache_files()
+    logging.info(f'Clean-up successful: {num_cleared_files} files cleared...')
 
 
 def preprocess_dataset(dataset_obj, maxSentLen):
     """Clean + tokenize text, filter on size, build vocab, find max sent length and encode. """
 
     logging.info('Cleaning and tokenizing HF dataset...')
+
     dataset_obj = dataset_obj.map(clean_tokenize_text, batched=False, load_from_cache_file=True)
 
     logging.info('Filtering on max word length...')
@@ -115,6 +52,7 @@ def preprocess_dataset(dataset_obj, maxSentLen):
     Test: {len(dataset_obj['test'])}")
 
     logging.info('Building vocabulary dict...')
+
     vocab_dict = {}
     vocab_dict['<pad>'] = 0
     vocab_dict['<unk>'] = 1
@@ -142,28 +80,118 @@ def preprocess_dataset(dataset_obj, maxSentLen):
 
     return dataset_obj, vocab_dict, max_sent_len
 
+
+def retrieve_word_vectors(data_dir, vec_url):
+    """ Grab FastText word vectors."""
+
+    vec_name = vec_url.split('/')[-1]
+
+    output_filepath = data_dir + '/' + vec_name
+
+    # check if word vectors already there
+    if os.path.isfile(output_filepath.replace('.zip', '')):
+
+        logging.info(f'Word vec file already found at {output_filepath} - skipping download...')
+
+    else:
+
+        # if not, download them from the FastText site
+        logging.info(f'Downloading Fasttext Vectors from {vec_url}...')
+
+        r = requests.get(vec_url, stream=True)
+        total_len = int(r.headers.get('content-length', 0))
+        block_size = 1024
+
+        # download in blocks using requests
+        with tqdm(total=total_len, unit='B', unit_scale=True) as progress_bar:
+            with open(output_filepath, 'wb') as f:
+                for data in r.iter_content(block_size):
+                    progress_bar.update(len(data))
+                    f.write(data)
+
+            if total_len != 0 and progress_bar.n != total_len:
+                logging.error("Could not download FastText Vectors")
+                raise RuntimeError("Could not download FastText Vectors")
+
+        logging.info('unzipping word vecs...')
+        shutil.unpack_archive(output_filepath, extract_dir=data_dir)
+
+        # clean up downloaded .zip file
+        try:
+            os.remove(output_filepath)
+        except:
+            logging.error(f'Unable to remove .zip file at {output_filepath}')
+
+    word_vec_path = output_filepath.replace('.zip', '')
+
+    return word_vec_path
+
+
 def load_word_vectors(vocab_dict, word_vector_filepath):
+    """ Loads FastText vectors from downloaded .vec file."""
 
     with open(word_vector_filepath, 'r', encoding='utf-8', newline='\n', errors='ignore') as f:
         vocab_dict_size, embedding_dim_size = map(int, f.readline().split())
 
-        # make random vectors first of all
+        # make random vectors first of all as starting point
+
         embeddings = np.random.uniform(-0.25, 0.25, (len(vocab_dict), embedding_dim_size))
         embeddings[vocab_dict['<pad>']] = np.zeros((embedding_dim_size,))
 
-        # load in pretrained vectors
+        # load in pretrained vectors for words that FastText has encodings for
+
         pretrained_count = 0
+
         for line in tqdm(f, total=vocab_dict_size):
+
             tokens = line.rstrip().split(' ')
             word = tokens[0]
+
             if word in vocab_dict:
                 pretrained_count += 1
                 embeddings[vocab_dict[word]] = np.array(tokens[1:], dtype=np.float32)
 
-    logging.info(f'There are {pretrained_count} / {len(vocab_dict)} pretrained vectors found from training data.')
+    logging.info(f'There are {pretrained_count} / {len(vocab_dict)} \
+                 FT pretrained vectors found from training data.')
 
-    return embeddings
+    return torch.tensor(embeddings)
 
+
+def clean_tokenize_text(example):
+    """ Adds cleaned + tokenized text to dataset example."""
+
+    # cleaning
+    norm_text = example['comment_text'].lower()
+    exclude = set(string.punctuation)
+    norm_text = re.sub(r"\[.*?]", "", norm_text)
+    norm_text = ''.join(ch for ch in norm_text if ch not in exclude)
+    norm_text = norm_text.replace('\n', '')
+    example['norm_comment_text'] = norm_text
+
+    # tokenizing
+    tokenized_text = fasttext.tokenize(example['norm_comment_text'])
+    example['tokenized_text'] = tokenized_text
+
+    # get sentence length for filtering later
+    example['sentWordCount'] = len(example['tokenized_text'])
+
+    return example
+
+
+def encode_text(example, vocab_dict, max_sent_len):
+    """Adds encoded text to dataset example ready for training."""
+
+    tokenized_text = example['tokenized_text']
+
+    # padding
+    tokenized_text += ['<pad>'] * (max_sent_len - len(tokenized_text))
+
+    # encoding the padded tokens to their IDs using the vocab dict
+    encoded_text = [vocab_dict.get(token, 1) for token in tokenized_text]
+
+    example['encoded_text'] = np.array(encoded_text)
+
+    return example
 
 
 class ToxicDataset(Dataset):
--- src/data_prep.py
+++ src/data_prep.py
@@ -1,48 +1,51 @@
-import argparse
 import logging
 import pickle
-from data_functions import *
+
+from data_functions import retrieve_dataset, clear_dataset_cache, preprocess_dataset
+from data_functions import retrieve_word_vectors, load_word_vectors
 from utils import configure_logging, load_json, config_parser
 
 def main():
 
-    # load config
     args = config_parser()
     config = load_json(args.config_path)
 
     configure_logging('data_prep')
     logging.info('Creating dataset files...')
 
-    # huggingface pulling + cleaning
-
     logging.info('Pulling HuggingFace Dataset...')
-    full_dataset = retrieve_dataset(config['hfDatasetName'], config['dataDir'])
-    del full_dataset['balanced_train']
 
-    logging.info('Cleaning and tokenizing...')
+    full_dataset = retrieve_dataset(config['hfDatasetName'], config['dataDir'])
+    del full_dataset['balanced_train']  # balanced_train not used
+
+    logging.info('Cleaning, tokenizing and building the vocabulary...')
+
     processed_dataset, vocab_dict, max_sent_len = preprocess_dataset(full_dataset, config['maxSentWordLen'])
 
-    # fastText pulling + mapping to huggingface vocab
-
     logging.info('Downloading FastText word vectors...')
-    word_vectors_path = retrieve_word_vecs(config['dataDir'], config['ftVectorUrl'])
+
+    word_vectors_path = retrieve_word_vectors(config['dataDir'], config['ftVectorUrl'])
 
     logging.info('Mapping FastText word vectors to existing vocab dict...')
-    word_embeddings = torch.tensor(load_word_vectors(vocab_dict, word_vectors_path))
+    word_embeddings = load_word_vectors(vocab_dict, word_vectors_path)
 
-    # saving files
-
-    logging.info('Saving HF dataset files to disk for easy loading for training')
+    logging.info('Saving processed HF dataset files to disk for easy loading for training')
 
     hf_dataset_path = config['dataDir'] + '/processed_dataset'
     word_emb_path = config['dataDir'] + '/word_embeddings.pkl'
 
     processed_dataset.save_to_disk(hf_dataset_path)
+
     with open(word_emb_path, 'wb') as f:
         pickle.dump(word_embeddings, f)
 
     logging.info(f'Processed HF dataset saved at {hf_dataset_path}...')
     logging.info(f'Final embeddings saved at {word_emb_path}')
+
+    if config['keepHfDatasetCached'] == False:
+        logging.info("Deleting HF cache as request...")
+        clear_dataset_cache(config['hfDatasetName'], config['dataDir'])
+
 
 if __name__ == "__main__":
     main()
