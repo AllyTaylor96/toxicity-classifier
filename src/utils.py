a497e523dbfd57949b34f64491caec4e2734f981
AllyTaylor96 <allytaylor@hotmail.co.uk>
2024-02-21 15:23:57 +0000

    adding docstrings and cleaning utils

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,21 @@
 import argparse
+import json
 import logging
 import os
-import json
 
 
 def configure_logging(logname):
+    """Sets up logger to have sensible format and to output to both file and console."""
+
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s',
         handlers=[
-            logging.FileHandler(f'logs/{logname}.log', mode='w'),  # Output to a file
-            logging.StreamHandler() # Output to console
+            logging.FileHandler(f'logs/{logname}.log', mode='w'),
+            logging.StreamHandler()
         ]
     )
+
 
 def load_json(json_path):
     with open(json_path, 'r') as f:
@@ -21,6 +24,8 @@ def load_json(json_path):
     return json_data
 
 def config_parser():
+    """Parses the config path given for every set of functionality."""
+
     parser = argparse.ArgumentParser(description='Config path reader for data prep')
     parser.add_argument('-c', '--config_path', type=str, help='Path to the config file')
     args = parser.parse_args()
